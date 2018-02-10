918ae85db3cc917ab0d6ccfc9caa4ae3fb732568
Christian Tavares <chrisadrianotav@gmail.com>
2018-02-10 18:46:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function takeANumber(line,name) {
   return `Welcome, ${name}. You are number ${line.length} in line.` //simple interpolation ${}
 }
 
-function nowServing(person) {
+function nowServing(line) {
   if(line.length === 0) {
     return `There is nobody waiting to be served!`
   }
